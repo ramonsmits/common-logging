bbf9a9e8c1c0a8a01e800425ab7693280dc1e88f
Ramon Smits <ramon@ramonsmits.com>
2012-06-21 15:17:01 +0200

    Added MultiFactory adapter. Enabled to use of multiple adapters to log an event to multiple logging frameworks.

---
+++ src/Common.Logging.MultiFactory/Logging/MultiFactory/MultiFactoryFactoryAdapter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common.Logging.MultiFactory
+{
+	/// <summary>
+	/// Multi Factory Logger Factory Adapter can use multiple logging factory
+	/// implementations to send a log item to multiple logging engines.
+	/// </summary>
+	public class MultiFactoryLoggerFactoryAdapter : ILoggerFactoryAdapter
+	{
+		/// <summary>
+		/// Registered Logger Factory Adapters.
+		/// </summary>
+		public readonly List<ILoggerFactoryAdapter> LoggerFactoryAdapters;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MultiFactoryLoggerFactoryAdapter"/> class.
+		/// </summary>
+		public MultiFactoryLoggerFactoryAdapter()
+		{
+			LoggerFactoryAdapters = new List<ILoggerFactoryAdapter>();
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MultiFactoryLoggerFactoryAdapter"/> class.
+		/// </summary>
+		/// <param name="factoryAdapters">The factory adapters.</param>
+		public MultiFactoryLoggerFactoryAdapter(List<ILoggerFactoryAdapter> factoryAdapters)
+		{
+			LoggerFactoryAdapters = factoryAdapters;
+		}
+
+		/// <summary>
+		/// Gets the logger.
+		/// </summary>
+		/// <param name="loggerName">Name of the logger.</param>
+		/// <returns></returns>
+		public ILog GetLogger(string loggerName)
+		{
+			var loggers = new List<ILog>(LoggerFactoryAdapters.Count);
+
+			foreach (var f in LoggerFactoryAdapters)
+			{
+				loggers.Add(f.GetLogger(loggerName));
+			}
+
+			return new MultiLogger(loggers);
+		}
+
+		/// <summary>
+		/// Get a ILog instance by type.
+		/// </summary>
+		/// <param name="type">The type to use for the logger</param>
+		/// <returns></returns>
+		public ILog GetLogger(Type type)
+		{
+			var loggers = new List<ILog>(LoggerFactoryAdapters.Count);
+
+			foreach (var f in LoggerFactoryAdapters)
+			{
+				loggers.Add(f.GetLogger(type));
+			}
+
+			return new MultiLogger(loggers);
+		}
+	}
+}
